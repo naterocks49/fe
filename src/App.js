4d98c7a20a0f4032c0baa378cc4fd7766a28504a
naterocks49 <108183408+naterocks49@users.noreply.github.com>
2023-04-18 12:08:12 -0400

    Added button comp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import Button from "./Button"
+
 function App() {
-    return <div>App</div>
+    return <div>
+        <div>
+            <Button success rounded outline>a</Button>
+        </div>
+        <div>
+            <Button danger outline>b</Button>
+        </div>
+        <div>
+            <Button warning rounded>c</Button>
+        </div>
+        <div>
+            <Button secondary outline>d</Button>
+        </div>
+    </div>
 };
 
 export default App
