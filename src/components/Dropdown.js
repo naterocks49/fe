7cca5f7f886c518b7ef9c3926e75b167deacbec6
naterocks49 <108183408+naterocks49@users.noreply.github.com>
2023-04-20 20:10:19 -0400

    Updates

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,18 +1,18 @@
 import { useState } from 'react';
 
-function Dropdown({ options }) {
+function Dropdown({ options, selection, handleSelect }) {
     const [isOpen, setIsOpen] = useState(false);
 
     const handleClick = () => {
         setIsOpen((currentIsOpen) => !currentIsOpen);
     };
 
-    const handleOptionClick = () => {
+    const handleOptionClick = (option) => {
         setIsOpen(false);
     };
 
     const renderedOptions = options.map((option) => {
-        return <div onClick={handleOptionClick} key={option.value}>
+        return <div onClick={() => handleOptionClick(option)} key={option.value}>
             {option.label}
         </div>
     });
