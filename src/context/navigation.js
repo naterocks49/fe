7eb84a67be5dbfc7aef6850efac0d451826614d9
naterocks49 <108183408+naterocks49@users.noreply.github.com>
2023-04-21 10:12:48 -0400

    Index import

--- src/context/navigation.js
+++ src/context/navigation.js
@@ -1,8 +1,10 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const NavigationContext = createContext();
 
 function NavigationProvider({ children }) {
+    const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
     return <NavigationContext.Provider value={{}}>
         {children}
     </NavigationContext.Provider>
