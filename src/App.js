e7e6109d29eb45e0d2de0b9b85683d164c3db6a9
naterocks49 <108183408+naterocks49@users.noreply.github.com>
2023-04-20 20:52:27 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -14,7 +14,12 @@ function App() {
         { label: 'Blue', value: 'blue' },
     ]
 
-    return <Dropdown options={options} value={selection} onChange={handleSelect} />;
+    return (
+        <div className='flex'>
+            <Dropdown options={options} value={selection} onChange={handleSelect} />
+            <Dropdown options={options} value={selection} onChange={handleSelect} />
+        </div>
+    );
 };
 
 export default App;
