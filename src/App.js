124336d9fc3a672507bfbd4d302aa7aded14522d
naterocks49 <108183408+naterocks49@users.noreply.github.com>
2023-04-21 10:57:28 -0400

    Updated alignemnt

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import ButttonPage from "./pages/ButtonPage";
 
 function App() {
     return (
-        <div>
+        <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
             <Sidebar />
-            <div>
+            <div className="col-span-5">
                 <Route path="/accordion">
                     <AccordionPage />
                 </Route>
