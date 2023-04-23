0722596fbaf521a61e17a526342b04d8912e3fb6
naterocks49 <108183408+naterocks49@users.noreply.github.com>
2023-04-23 16:18:01 -0400

    Added data

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,29 @@
 function Table({ data }) {
-    return <div>{data.length}</div>
+
+    const renderedData = data.map((row) => {
+        return (
+            <tr key={row.name}>
+                <td>{row.name}</td>
+                <td>{row.color}</td>
+                <td>{row.score}</td>
+            </tr>
+        );
+    });
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Fruit</th>
+                    <th>Color</th>
+                    <th>Score</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderedData}
+            </tbody>
+        </table>
+    );
 };
 
-export default Table
+export default Table;
