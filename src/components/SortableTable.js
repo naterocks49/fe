283d7db1e8d923e095d24c22b12eb76db6b6d0b1
naterocks49 <108183408+naterocks49@users.noreply.github.com>
2023-05-04 08:59:15 -0400

    sorting

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,7 +1,36 @@
 import Table from "./Table";
+import { useState } from "react";
 
 function SortableTable(props) {
-    return <Table {...props}/>;
+    const [sortOrder, setSortOrder] = useState(null);
+    const [sortBy, setSortBy] = useState(null);
+    const { config } = props;
+
+    const handleClick = (label) => {
+        if (sortOrder === null) {
+            setSortOrder('asc');
+            setSortBy(label);
+        } else if (sortOrder === 'asc') {
+            setSortOrder('desc');
+            setSortBy(label);
+        } else if (sortOrder === 'desc') {
+            setSortOrder(null);
+            setSortBy(null);
+        }
+    };
+
+    const updatedConfig = config.map((column) => {
+        if (!column.sortValue) {
+            return column
+        };
+
+        return {
+            ...column,
+            header: () => <th onClick={() => handleClick(column.label)}>{column.label} IS SORTABLE</th>
+        };
+    });
+
+    return <Table {...props} config={updatedConfig} />;
 };
 
 export default SortableTable;
