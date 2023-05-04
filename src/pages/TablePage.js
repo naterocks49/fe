4576176be79ed942da977730c6e6adf1950aba08
naterocks49 <108183408+naterocks49@users.noreply.github.com>
2023-05-04 08:42:50 -0400

    fix

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,4 +1,5 @@
-import Table from '../components/Table';
+import Table from "../components/Table";
+import SortableTable from "../components/SortableTable";
 
 function TablePage() {
     const data = [
@@ -11,7 +12,8 @@ function TablePage() {
     const config = [
         {
             label: 'Name',
-            render: (fruit) => fruit.name
+            render: (fruit) => fruit.name,
+            sortValue: (fruit) => fruit.name
         },
         {
             label: 'Color',
@@ -19,7 +21,8 @@ function TablePage() {
         },
         {
             label: 'Score',
-            render: (fruit) => fruit.score
+            render: (fruit) => fruit.score,
+            sortValue: (fruit) => fruit.score
         },
     ];
 
@@ -29,7 +32,7 @@ function TablePage() {
 
     return (
         <div>
-            <Table data={data} config={config} keyFn={keyFn} />
+            <SortableTable data={data} config={config} keyFn={keyFn} />
         </div>
     );
 };
